7407227a83b6608e3d5bf6ef778813cec462e642
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-09-11 02:14:29 +0900

    Added sample reporting line.

--- extra/agent.sh
+++ extra/agent.sh
@@ -2,7 +2,13 @@
 
 # Print "Hello, World!" to the console every 5 seconds as a background 
 # Making sure this continues after the script completes execution (nohup, and disown process)
+echo "Hello, World!"
+
+# Dump all the environment variables to the console:
+echo "Environment variables:"
+printenv
+
 while true; do
-    echo "Hello, World!"
-    sleep 5
+    echo "source=web.1 dyno=heroku.2808254.d97d0ea7-cf3d-411b-b453-d2943a50b456 sample#load_avg_1m=2.46 sample#load_avg_5m=1.06 sample#load_avg_15m=0.99"
+    sleep 20
 done &
