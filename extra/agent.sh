697bb93d25adcef14227d55766f81525e817c6ae
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2024-09-11 21:47:39 +0900

    Matching versions...

--- extra/agent.sh
+++ extra/agent.sh
@@ -2,7 +2,7 @@
 
 # Print "Hello, World!" to the console every 5 seconds as a background 
 # Making sure this continues after the script completes execution (nohup, and disown process)
-echo "Starting Dyno Metrics Agent... v0.1"
+echo "Starting Dyno Metrics Agent... v0.02"
 
 # Dump all the environment variables to the console:
 echo "Environment variables:"
